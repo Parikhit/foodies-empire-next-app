dfe72f1ebfe495fee1c388d440e76c49256a8717
Parikhit <parikhitritzz@gmail.com>
2024-01-22 23:20:32 +0530

    Adding share recipe feature with form submission status

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,8 @@
 import ImagePicker from '@/components/image-picker/image-picker.component';
 import { shareMeal } from '@/lib/action';
 
+import MealsFormSubmit from '@/components/meals-form-submit/meals-form-submit.component';
+
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
@@ -43,7 +45,7 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label='Your Image' name='image' />
                     <p className={classes.actions}>
-                        <button type='submit'>Share Meal</button>
+                        <MealsFormSubmit />
                     </p>
                 </form>
             </main>
